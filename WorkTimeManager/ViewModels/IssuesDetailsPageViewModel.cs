09858ed4b5e5466f334791971d2088d9da6b2e96
Gáspár Vilmos <gaspar.vilmos24@gmail.com>
2017-10-10 15:15:48 +0200

    issues details page added

---
+++ WorkTimeManager/ViewModels/IssuesDetailsPageViewModel.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Template10.Mvvm;
+using WorkTimeManager.Bll.Interfaces;
+using WorkTimeManager.Bll.Services;
+using WorkTimeManager.Model.Models;
+using WorkTimeManager.Models;
+
+namespace WorkTimeManager.ViewModels
+{
+    class IssuesDetailsPageViewModel : ViewModelBase
+    {
+
+        public static readonly int SubjectKey = 0;
+        public static readonly int ProjectNameKey = 1;
+        public static readonly int TrackerKey = 2;
+
+        private ObservableCollection<IssueTime> list;
+        public ObservableCollection<IssueTime> List
+        {
+            get { return list; }
+            set { Set(ref list, value); }
+        }
+
+        IIssueService issueService;
+
+        public IssuesDetailsPageViewModel()
+        {
+
+            issueService = IssueService.Instance;
+
+            List = new ObservableCollection<IssueTime>();
+            Refresh();
+        }
+
+        public async void Refresh()
+        {
+            var issueList = await issueService.GetFavouriteIssues();
+            List = new ObservableCollection<IssueTime>(issueList.Select(i => new IssueTime(i, i.WorkTimes.Sum(t => t.Hours))).ToList());
+        }
+
+        private int orderCatName;
+        public int OrderCatName
+        {
+            get { return orderCatName; }
+            set { orderCatName = value; }
+        }
+        public void OrderCats(bool byDesc)
+        {
+            switch (OrderCatName)
+            {
+                case 0:
+                    if (byDesc)
+                    { List = new ObservableCollection<IssueTime>(List.OrderByDescending(i => i.Subject)); }
+                    else
+                    { List = new ObservableCollection<IssueTime>(List.OrderBy(i => i.Subject)); }
+                    break;
+                case 1:
+                    if (byDesc)
+                    { List = new ObservableCollection<IssueTime>(List.OrderByDescending(i => i.Project.Name)); }
+                    else
+                    { List = new ObservableCollection<IssueTime>(List.OrderBy(i => i.Project.Name)); }
+                    break;
+                case 2:
+                    if (byDesc)
+                    { List = new ObservableCollection<IssueTime>(List.OrderByDescending(i => i.Tracker)); }
+                    else
+                    { List = new ObservableCollection<IssueTime>(List.OrderBy(i => i.Tracker)); }
+                    break;
+            }
+        }
+
+    }
+}
---
+++ WorkTimeManager/Views/IssuesDetailsPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace WorkTimeManager.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class IssuesDetailsPage : Page
+    {
+        public IssuesDetailsPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private bool OrderbyDesc = false;
+        private void OrderData(object sender, TappedRoutedEventArgs e)
+        {
+            TextBlock ClickedBox = sender as TextBlock;
+            SetNormalOpícity();
+            ClickedBox.Opacity = 0.6;
+            switch (ClickedBox.Name)
+            {
+                case "Subject":
+                    ViewModel.OrderCatName = ViewModels.IssuesDetailsPageViewModel.SubjectKey;
+                    break;
+                case "ProjectName":
+                    ViewModel.OrderCatName = ViewModels.IssuesDetailsPageViewModel.ProjectNameKey;
+                    break;
+                case "Tracker":
+                    ViewModel.OrderCatName = ViewModels.IssuesDetailsPageViewModel.TrackerKey;
+                    break;
+            }
+            ViewModel.OrderCats(OrderbyDesc);
+            OrderbyDesc = !OrderbyDesc;
+        }
+
+        private void SetNormalOpícity()
+        {
+            Subject.Opacity = 1;
+            ProjectName.Opacity = 1;
+            Tracker.Opacity = 1;
+        }
+    }
+}
