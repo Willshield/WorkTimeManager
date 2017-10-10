7263c0f380ab0f3866af6251b6132e2531ef57b2
Gáspár Vilmos <gaspar.vilmos24@gmail.com>
2017-10-10 17:42:47 +0200

    settings and issue fix

--- WorkTimeManager/Models/IssueTime.cs
+++ WorkTimeManager/Models/IssueTime.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WorkTimeManager.Bll.Services;
 using WorkTimeManager.Model.Models;
 
 namespace WorkTimeManager.Models
@@ -14,7 +15,16 @@ namespace WorkTimeManager.Models
         public string Subject { get; set; }
         public string Description { get; set; }
         public bool Dirty { get; set; }
-        public bool IsFavourite { get; set; }
+        private bool isFavourite { get; set; }
+        public bool FavouriteSetter
+        {
+            get { return this.isFavourite; }
+            set {
+                isFavourite = value;
+                Task.Run(() => { IssueService.Instance.SetFavourite(this.IssueID, value); });
+            }
+        }
+
         public string Priority { get; set; }
         public DateTime Updated { get; set; }
 
@@ -22,6 +32,8 @@ namespace WorkTimeManager.Models
         public int ProjectID { get; set; }
         public Project Project { get; set; }
         public double AllTrackedTime { get; set; }
+
+
 
         public IssueTime()
         {
@@ -35,7 +47,7 @@ namespace WorkTimeManager.Models
             Subject = issue.Subject;
             Description = issue.Description;
             Dirty = issue.Dirty;
-            IsFavourite = issue.IsFavourite;
+            isFavourite = issue.IsFavourite;
             Priority = issue.Priority;
             Updated = issue.Updated;
             ProjectID = issue.ProjectID;
@@ -52,7 +64,7 @@ namespace WorkTimeManager.Models
                 Subject = this.Subject,
                 Description = this.Description,
                 Dirty = this.Dirty,
-                IsFavourite = this.IsFavourite,
+                IsFavourite = this.isFavourite,
                 Priority = this.Priority,
                 Updated = this.Updated,
                 ProjectID = this.ProjectID,
